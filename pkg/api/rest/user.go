7b47acb2f73b2ebca8ec79dd7b0c0711d782a801
Bach Le Tuan Khai <leetuankhai.bach@hmgics.com>
2025-05-15 17:57:17 +0800

    wip: Implement UserController and add JWT token generation functionality

--- pkg/api/rest/user.go
+++ pkg/api/rest/user.go
@@ -1,1 +1,11 @@
 package rest
+
+import (
+	"locket-clone/backend/pkg/service/adding"
+	"locket-clone/backend/pkg/service/auth"
+)
+
+type UserController struct {
+	UserAddingService adding.UserService
+	AuthService       auth.AuthService
+}
--- pkg/service/auth/auth.go
+++ pkg/service/auth/auth.go
@@ -3,6 +3,7 @@ package auth
 import (
 	"errors"
 	"os"
+	"time"
 
 	"github.com/golang-jwt/jwt/v5"
 	"golang.org/x/crypto/bcrypt"
@@ -28,7 +29,7 @@ func ParseToken(token string) (JwtClaims, error) {
 
 	res, err := jwt.ParseWithClaims(token, &claims, func(t *jwt.Token) (interface{}, error) {
 		return os.ReadFile("./public.key")
-	}, jwt.WithValidMethods([]string{"RS256","RS384","RS512"}))
+	}, jwt.WithValidMethods([]string{"RS256", "RS384", "RS512"}))
 	if err != nil {
 		return claims, err
 	}
@@ -37,4 +38,110 @@ func ParseToken(token string) (JwtClaims, error) {
 		return claims, nil
 	}
 	return claims, errors.New("Unauthorized")
+}
+
+func GenerateToken(username string) (string, error) {
+	claims := JwtClaims{
+		Username: username,
+		RegisteredClaims: jwt.RegisteredClaims{
+			Issuer:    "",
+			Subject:   "",
+			Audience:  jwt.ClaimStrings{},
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(1 * time.Hour)),
+			NotBefore: jwt.NewNumericDate(time.Now()),
+			IssuedAt:  jwt.NewNumericDate(time.Now()),
+			ID:        "",
+		},
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodRS512, claims)
+	key, err := os.ReadFile("./private.key")
+	if err != nil {
+		return "", err
+	}
+	signedToken, err := token.SignedString(key)
+	if err != nil {
+		return "", err
+	}
+	return signedToken, nil
+}
+
+func GenerateRefreshToken(username string) (string, error) {
+	claims := JwtClaims{
+		Username: username,
+		RegisteredClaims: jwt.RegisteredClaims{
+			Issuer:    "",
+			Subject:   "",
+			Audience:  jwt.ClaimStrings{},
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(24 * time.Hour)),
+			NotBefore: jwt.NewNumericDate(time.Now()),
+			IssuedAt:  jwt.NewNumericDate(time.Now()),
+			ID:        "",
+		},
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodRS512, claims)
+	key, err := os.ReadFile("./private.key")
+	if err != nil {
+		return "", err
+	}
+	signedToken, err := token.SignedString(key)
+	if err != nil {
+		return "", err
+	}
+	return signedToken, nil
+}
+
+type Token struct {
+	Auth    string `json:"auth"`
+	Refresh string `json:"refresh"`
+}
+
+type AuthService interface {
+	Login(username, password string) (Token, error)
+	// RefreshToken(token string) (Token, error)  // TODO: Implement refresh token
+	Logout(token string) error
+}
+
+type TokenRepo interface {
+	SaveToken(token string, exp time.Time) error
+	CheckToken(token string) (bool, error)
+}
+
+type UserRepo interface {
+	GetUserPasswordHashByUsername(username string) (string, error)
+}
+
+type authService struct {
+	TokenRepo TokenRepo
+	UserRepo  UserRepo
+}
+
+func (s *authService) Login(username string, password string) (Token, error) {
+	hash, err := s.UserRepo.GetUserPasswordHashByUsername(username)
+	if err != nil {
+		return Token{}, err
+	}
+	if !CompareHashPassword(password, hash) {
+		return Token{}, errors.New("Invalid password")
+	}
+
+	token, err := GenerateToken(username)
+	if err != nil {
+		return Token{}, err
+	}
+
+	refreshToken, err := GenerateRefreshToken(username)
+	if err != nil {
+		return Token{}, err
+	}
+
+	return Token{
+		Auth:    token,
+		Refresh: refreshToken,
+	}, nil
+
+}
+
+// RefreshToken(token string) (Token, error)  // TODO: Implement refresh token
+func (s *authService) Logout(token string) error {
+	panic("not implemented") // TODO: Implement
 }
